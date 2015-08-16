a713ca38bfbf96eb6b9cf091cc7c46d1ef1d8c21
Joshua Galvin <joshuagalvin@gmail.com>
2015-08-15 21:18:52 -0700

    first

---
+++ Assets/RenderEntity.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Json;
+
+public struct EntityComponent {
+	public JsonObject data;
+	public GameObject gameObject;
+
+	public EntityComponent(JsonObject p1, GameObject p2) {
+		data = p1;
+		gameObject = p2;
+	}
+}
+
+public class RenderEntity : MonoBehaviour {
+	public TextAsset entityDef;
+	public GameObject nodeType;
+
+	private	JsonObject data;
+	private Dictionary<string, EntityComponent> euids = new Dictionary<string, EntityComponent>();
+
+	// Use this for initialization
+	void Start () {
+		data = (JsonObject)JsonValue.Parse (entityDef.ToString ());	
+
+		// consider making these joints
+		foreach(JsonObject node in data["nodes"]) {
+			GameObject obj = (GameObject)Object.Instantiate(nodeType, 
+			                                    new Vector3(node["pos"][0], node["pos"][1], node["pos"][2]), 
+			                                    new Quaternion());
+
+			obj.transform.parent = transform;
+			euids[node["euid"]] = new EntityComponent(node, obj);
+		}
+
+		// Yay add sprite data
+		foreach (JsonObject bone in data["bones"]) {
+			EntityComponent from = euids[bone["from"]];
+			EntityComponent to = euids[bone["to"]];
+
+			GameObject boneObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+			boneObject.transform.position = from.gameObject.transform.position;			                                                                
+			boneObject.transform.LookAt(to.gameObject.transform.position);
+			boneObject.transform.localScale.Set(1, 1, Vector3.Distance(from.gameObject.transform.position, to.gameObject.transform.position));
+			boneObject.transform.parent = transform;
+
+			euids[bone["euid"]] = new EntityComponent(bone, boneObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/StsTest.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using ArenaNet.Medley.Pool;
+using System.Xml.Serialization;
+using ArenaNet.Sts.Client;
+
+public class StsTest : MonoBehaviour {
+    private static readonly Uri EcosystemUri = new Uri("wss://cligate-dfw-2002.ncplatform.net/websocket");
+    private static readonly RemoteCertificateValidationCallback AllTrustingValidationCallback =
+      (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+      { return true; };
+
+    private static readonly ObjectPool<byte[]> BufferPool = new ObjectPool<byte[]>(() => { return new byte[1024]; });
+    public string message = "Bloop";
+
+    [XmlRoot("Request")]
+    public class BlankRequest
+    {
+        public string Message;
+    }
+
+    [XmlRoot("Request")]
+    public class AuthLoginRequest
+    {
+        public string Provider { set; get; }        // The provider of the given login
+        public string LoginName { set; get; }       // The username
+        public string Password { set; get; }        // The password encoded in Base64
+    }
+
+    [XmlRoot("Message")]
+    public class GenericResponse
+    {
+        public string Message;
+    }
+
+    // Use this for initialization
+    void Start() {
+        using (StsWebSocketClient client = new StsWebSocketClient(EcosystemUri, true, AllTrustingValidationCallback)
+                .Connect().WaitForValue(TimeSpan.FromSeconds(5)))
+        {
+
+            
+            StsRequestMessage loginRequest = StsRequestMessage.Create(BufferPool, "P", "/Auth/Login");
+            loginRequest.TransactionId = new StsTransactionId("1");
+            loginRequest.SetBodyAs(new AuthLoginRequest()
+            {
+                Provider = "Portal",
+                LoginName = "mpbtest@arena.net",
+                Password = "bXBidGVzdA=="
+            });
+
+            client.SendRequest(loginRequest).WaitForValue(TimeSpan.FromSeconds(5));
+            
+
+            StsRequestMessage request = StsRequestMessage.Create(BufferPool, "P", "/Game.lot.MyJsProto/Command");
+            request.TransactionId = new StsTransactionId("2");
+            request.SetBodyAs(new BlankRequest()
+            {
+                Message = message
+            });
+
+            request.Headers["connType"] = new[] { "401" };
+            request.Headers["buildId"] = new[] { "1000" };
+            request.Headers["programId"] = new[] { "2002" };
+            request.Headers["host"] = new[] { "@ExGameGate" };
+
+            StsResponseMessage response = client.SendRequest(request).WaitForValue(TimeSpan.FromSeconds(5));
+            if (response != null)
+            {
+                GenericResponse gotSomething = response.ReadBodyAs<GenericResponse>();
+                Debug.Log(gotSomething.Message);
+            } else
+            {
+                Debug.Log("Message timed out");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
